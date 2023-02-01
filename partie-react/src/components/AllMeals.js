6d9599b0c58f55d50f00063148a9d6b22f20bf1b
Maatyss <mvancaemelbecke@gmail.com>
2023-02-01 12:34:28 +0100

    Card Hover Handling

--- partie-react/src/components/AllMeals.js
+++ partie-react/src/components/AllMeals.js
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import axios from 'axios'
+import slugify from 'slugify'
 
 const AllMeals = ()=>{
   let [meals, setMeals] = useState([])
@@ -7,13 +8,25 @@ const AllMeals = ()=>{
     setMeals(response.data)
   })
   
+  const hoverHandle = (cardId, buttonId)=>{
+    let card = document.querySelector('#'+cardId)
+    let orderButton = document.querySelector('#'+buttonId)
+    card.classList.toggle('hovered')
+    orderButton.classList.toggle('hidden')
+  }
   
   return(
     <div className={'flex allmeals'}>
   
       {meals.map((meal, index)=>{
+        let cardId = slugify(meal.title)
+        let buttonId = slugify(meal.title) + '_Button'
         return(
-          <article className={'card'} key={index}>
+          <article id={cardId} className={'card'} key={index}
+                   onMouseEnter={() => hoverHandle(cardId, buttonId)}
+                   onMouseLeave={() => hoverHandle(cardId, buttonId)}
+          
+          >
             <img className={'foodpic'} src={meal.imageSrc} alt={meal.title}/>
             <div>
               <h3 className={'title'}>{meal.title}</h3>
@@ -21,6 +34,8 @@ const AllMeals = ()=>{
               <h4 className={'price'}>{meal.price}</h4>
             </div>
             <p className={'rating'}>⭐{meal.rating}<span className={'reviews'}>({meal.reviews})</span></p>
+            <p id={buttonId} className={'orderButton hidden'}>Commander</p>
+            <div className={'greyfilter'}></div>
           </article>
         )
       })}
