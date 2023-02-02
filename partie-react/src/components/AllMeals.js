42e5e64c1564484357b59c8cd4706e56203dd01e
Maatyss <mvancaemelbecke@gmail.com>
2023-02-02 10:58:03 +0100

    Fix Filter

--- partie-react/src/components/AllMeals.js
+++ partie-react/src/components/AllMeals.js
@@ -1,23 +1,35 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import axios from 'axios'
 import {SingleMeal} from './SingleMeal'
 
-const AllMeals = (props)=>{
+const AllMeals = (props)=> {
   let [meals, setMeals] = useState([])
   let [meal, setMeal] = useState([])
+  let [filteredMeals, setFilteredMeals] = useState([])
   
-  axios.get('http://localhost:3000/meals').then(response =>{
-    let filteredMeals = response.data
-    if(props.catToDisplay !== 'all'){
-      filteredMeals = response.data.filter(meal => meal.type.includes(props.catToDisplay))
+  
+  useEffect(() => {
+    axios.get('http://localhost:3000/meals').then(response => {
+      setMeals(response.data)
+      setFilteredMeals(response.data)
+    })
+  }, [])
+  
+  useEffect(() => {
+    console.log('ici')
+    console.log(props.catToDisplay)
+    if (props.catToDisplay !== 'all') {
+      let filtered = meals.filter(meal => meal.type.includes(props.catToDisplay))
+      setFilteredMeals(filtered)
+    } else {
+      setFilteredMeals(meals)
     }
-    setMeals(filteredMeals)
-  })
+    setFilteredMeals(filteredMeals)
+  }, [props.catToDisplay])
   
   
-  
-  const DisplayMealChoice = ()=>{
-    return(
+  const DisplayMealChoice = () => {
+    return (
       <div className={'choosenOne'}>
         <h3 className={'choosenMeal'}>Vous avez commandé le repas {meal.title}</h3>
         <img src={meal.imageSrc} alt={meal.title}/>
@@ -33,9 +45,9 @@ const AllMeals = (props)=>{
   return(
       props.displayAll &&
       <div className={'flex allmeals'}>
-        {meals.map((meal, index) => (
-                     <SingleMeal key={index} meal={meal} click={handleClick}/>
-                   )
+        {filteredMeals.map((meal, index) => (
+                             <SingleMeal key={index} meal={meal} click={handleClick}/>
+                           ),
         )}
       </div>
           || <DisplayMealChoice />
